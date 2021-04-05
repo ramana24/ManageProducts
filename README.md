1b1456b3a86c2ea3d795be03548de69216c3ca8d
ramana24 <yellapuramana@gmail.com>
2021-04-05 23:39:28 +0530

    Added supplier table in details page

---
+++ README.md
@@ -0,0 +1,35 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Mon Apr 05 2021 08:59:32 GMT+0530 (India Standard Time)|
+|**App Generator**<br>@sap/generator-fiori|
+|**App Generator Version**<br>1.1.7|
+|**Generation Platform**<br>Visual Studio Code|
+|**Floorplan Used**<br>2masterdetail|
+|**Service Type**<br>OData Url|
+|**Service URL**<br>https://sapes5.sapdevcenter.com/sap/opu/odata/iwbep/GWSAMPLE_BASIC
+|**Module Name**<br>viewproducts|
+|**Application Title**<br>Manage Products|
+|**Namespace**<br>ux|
+|**UI5 Theme**<br>sap_fiori_3|
+|**UI5 Version**<br>Latest|
+|**Enable Telemetry**<br>True|
+
+## viewproducts
+
+Customer,Supplier,Product Details comparison
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ webapp/test/flpSandbox.js
@@ -0,0 +1,100 @@
+sap.ui.define([
+	"sap/base/util/ObjectPath",
+	"sap/ushell/services/Container"
+], function (ObjectPath) {
+	"use strict";
+
+	// define ushell config
+	ObjectPath.set(["sap-ushell-config"], {
+		defaultRenderer: "fiori2",
+		bootstrapPlugins: {
+			"RuntimeAuthoringPlugin": {
+				component: "sap.ushell.plugins.rta",
+				config: {
+					validateAppVersion: false
+				}
+			},
+			"PersonalizePlugin": {
+				component: "sap.ushell.plugins.rta-personalize",
+				config: {
+					validateAppVersion: false
+				}
+			}
+		},
+		renderers: {
+			fiori2: {
+				componentData: {
+					config: {
+						enableSearch: false,
+						rootIntent: "Shell-home"
+					}
+				}
+			}
+		},
+		services: {
+			"LaunchPage": {
+				"adapter": {
+					"config": {
+						"groups": [{
+							"tiles": [{
+								"tileType": "sap.ushell.ui.tile.StaticTile",
+								"properties": {
+									"title": "Manage Products",
+									"targetURL": "#ManageProducts-display"
+								}
+							}]
+						}]
+					}
+				}
+			},
+			"ClientSideTargetResolution": {
+				"adapter": {
+					"config": {
+						"inbounds": {
+							"ManageProducts-display": {
+								"semanticObject": "ManageProducts",
+								"action": "display",
+								"description": "Customer,Supplier,Product Details comparison",
+								"title": "Manage Products",
+								"signature": {
+									"parameters": {}
+								},
+								"resolutionResult": {
+									"applicationType": "SAPUI5",
+									"additionalInformation": "SAPUI5.Component=ux.viewproducts",
+									"url": sap.ui.require.toUrl("ux/viewproducts")
+								}
+							}
+						}
+					}
+				}
+			},
+			NavTargetResolution: {
+				config: {
+					"enableClientSideTargetResolution": true
+				}
+			}
+		}
+	});
+
+	var oFlpSandbox = {
+		init: function () {
+			/**
+			 * Initializes the FLP sandbox
+			 * @returns {Promise} a promise that is resolved when the sandbox bootstrap has finshed
+			 */
+
+			// sandbox is a singleton, so we can start it only once
+			if (!this._oBootstrapFinished) {
+				this._oBootstrapFinished = sap.ushell.bootstrap("local");
+				this._oBootstrapFinished.then(function () {
+					sap.ushell.Container.createRenderer().placeAt("content");
+				});
+			}
+
+			return this._oBootstrapFinished;
+		}
+	};
+
+	return oFlpSandbox;
+});
